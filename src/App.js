fb060b8040f76bee6a34a481dcfa74bb173fa8e8
hackershil <hackershil@github.com>
2024-06-07 16:36:09 +0530

    Icon relocated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { Helmet } from "react-helmet";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App">Home Page </div>;
+  return (
+    <div className="App">
+      <Helmet>{/* Step 1: Add the appropriate description */}</Helmet>
+      <Header />
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      Navbar
+      <ui>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contacts</li>
+        <li>Reservation</li>
+      </ui>
+    </nav>
+  );
+};
+
+export default Navbar;
